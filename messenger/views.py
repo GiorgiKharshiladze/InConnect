c88af527b997e7af59079e0f123ef2ed62abcb24
DavitKvartskhava <dkvart17@earlham.edu>
2018-03-03 18:51:04 -0500

    yleobebi

--- messenger/views.py
+++ messenger/views.py
@@ -1,12 +1,27 @@
+import json, requests, random, re
+from pprint import pprint
+
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
-
+from django.views import generic
 # import rest_framework
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 from messenger.serializers import *
+
+VERIFY_TOKEN = 'MY_TOKEN_InConnect'
+class InConnectBotView(generic.View):
+
+	def get(self, request, *args, **kwargs):
+		if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
+			return HttpResponse(self.request.GET['hub.challenge'])
+		else:
+			return HttpResponse('Error, invalid token')
 
 # Create your views here.
 def api(request):
